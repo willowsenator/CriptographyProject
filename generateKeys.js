cedd7623a3499962b8a33a77d68401fd01803340
willowsenator <willowsenator@gmail.com>
2023-11-02 23:07:42 +0100

    Add elyptic curve algorithm to generate keys

---
+++ generateKeys.js
@@ -0,0 +1,22 @@
+const {createECDH} =  require("crypto");
+const args = require("yargs").argv;
+const fs = require("fs");
+
+const curveName = "secp521r1";
+
+if (args.name){
+    console.log("Creating ECDH object...");
+    const ecdh = createECDH(curveName);
+
+    console.log("Creating public and private keys...");
+    const publicKey = ecdh.generateKeys("hex");
+    const privateKey = ecdh.getPrivateKey("hex");
+    
+    console.log("Writing to file the public and private keys...");
+    fs.writeFileSync("./data/" + args.name + ".pb", publicKey);
+    fs.writeFileSync("./data/" + args.name + ".key", privateKey);
+
+} else {
+    console.error("Use: node generateKeys.js --name <custom_name>");
+    process.exit(0);
+}
